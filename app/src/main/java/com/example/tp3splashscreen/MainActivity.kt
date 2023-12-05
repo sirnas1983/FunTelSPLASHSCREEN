d07abb777826fcd1d149bbb5373b0a00895dc663
Darian Hiebl <dahiebl@gmail.com>
2023-12-05 17:18:13 -0300

    giro de icono y fade de titulo

--- app/src/main/java/com/example/tp3splashscreen/MainActivity.kt
+++ app/src/main/java/com/example/tp3splashscreen/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Thread.sleep(5000)
         setContentView(R.layout.activity_main)
     }
 }
--- app/src/main/java/com/example/tp3splashscreen/SplashScreen.kt
+++ app/src/main/java/com/example/tp3splashscreen/SplashScreen.kt
@@ -1,19 +1,41 @@
 package com.example.tp3splashscreen
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.view.Window
 import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        actionBar?.hide()
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         setContentView(R.layout.activity_splash_screen)
-        val i = Intent(this, MainActivity::class.java)
-        startActivity(i)
+
+        //Animacion
+        var ANIMATION_DURATION = 4500L
+        var icono : ImageView = findViewById(R.id.imageView)
+        val r: RotateAnimation =
+            RotateAnimation(0F, 360F, Animation.RELATIVE_TO_SELF, .5f,
+                Animation.RELATIVE_TO_SELF, .5f)
+        r.duration = ANIMATION_DURATION
+        r.repeatCount = 0
+        icono.startAnimation(r)
+        val f: TextView = findViewById(R.id.textView)
+        f.animate().alpha(0F).duration = ANIMATION_DURATION
+        //
+
+        Handler().postDelayed({
+            val i = Intent(this, MainActivity::class.java)
+            finish()
+            startActivity(i)
+        }, 5000)
     }
 }
