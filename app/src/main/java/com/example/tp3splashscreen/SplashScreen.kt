34b1421340789c7b4b6bcd66c6bb64791997abd7
Darian Hiebl <dahiebl@gmail.com>
2023-12-07 09:48:43 -0300

    Ultimas correcciones

--- app/src/main/java/com/example/tp3splashscreen/SplashScreen.kt
+++ app/src/main/java/com/example/tp3splashscreen/SplashScreen.kt
@@ -4,8 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
-import android.view.animation.Animation
-import android.view.animation.RotateAnimation
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -14,28 +13,31 @@ import androidx.appcompat.app.AppCompatActivity
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Configuraciones para ocultar la barra de notificaciones
         actionBar?.hide()
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         setContentView(R.layout.activity_splash_screen)
-
-        //Animacion
-        var ANIMATION_DURATION = 4500L
-        var icono : ImageView = findViewById(R.id.imageView)
-        val r: RotateAnimation =
-            RotateAnimation(0F, 360F, Animation.RELATIVE_TO_SELF, .5f,
-                Animation.RELATIVE_TO_SELF, .5f)
-        r.duration = ANIMATION_DURATION
-        r.repeatCount = 0
-        icono.startAnimation(r)
-        val f: TextView = findViewById(R.id.textView)
-        f.animate().alpha(0F).duration = ANIMATION_DURATION
-        //
-
+        // Metodo para atrasar el inicio del MainActivity
         Handler().postDelayed({
             val i = Intent(this, MainActivity::class.java)
             finish()
             startActivity(i)
         }, 5000)
+
+        // Animacion de ImageView y TextView
+        var icono : ImageView = findViewById(R.id.imageView)
+
+        var animation = AnimationUtils.loadAnimation(
+            getApplicationContext(),
+            R.anim.splash_screen_icon_animation
+        );
+
+        icono.startAnimation(animation)
+
+        val f: TextView = findViewById(R.id.textView)
+        f.animate().alpha(0F).duration = 4500
+
+
     }
 }
